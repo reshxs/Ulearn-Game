0e116cea803d3ec9aaab9db5bd8356f79e99e9fe
reshxs <andr.emeljanov2014@gmail.com>
2020-05-11 18:59:01 +0400

    Bullet

---
+++ Game/Bullet.cs
@@ -0,0 +1,19 @@
+﻿namespace Game
+{
+    public class Bullet
+    {
+        public const int Width = 10;
+        public const int Height = 30;
+        private const int Speed = 15;
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public Bullet(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void Move() => Y -= Speed;
+    }
+}
